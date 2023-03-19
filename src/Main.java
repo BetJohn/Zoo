5bf8bfba2d3b37e638ea4a5193d00fe091ebb77d
BetJohn <88667779+BetJohn@users.noreply.github.com>
2023-03-19 23:41:10 +0200

    Created Nemo

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         Bird bird1 = new Bird("Radu",7,"Colibri",200,true,true,"Green");
         if(bird1.canFly())
             System.out.println(bird1.getMaxAltitude());
+        Fish fish1 = new Fish("Nemo",2,"Clown",false,20,"sweet",800);
+        if(fish1.getName().equals("Nemo"))
+            System.out.println("We are looking for Nemo!");
     }
 }
